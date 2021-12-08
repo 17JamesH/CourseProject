022f3c61dce338847cf427f748fdb8f800b1de82
albertchongus <chongx@oregonstate.edu>
2021-12-07 19:17:48 -0800

    v4
    Simplify the web

--- frontend_js/src/Home/Components/NewListing.js
+++ frontend_js/src/Home/Components/NewListing.js
@@ -13,44 +13,36 @@ const NewListing = () =>{
                 <div className="grid">
                     <div className="grid-item featured-house">
                         <img src={"../../../images/house1.jpg"} alt={""} className="house-image"/>
-                            <h3 className="sale-price">From $500k</h3>
+                            <h3 className="sale-price">$ 899,000+</h3>
                             <div className="bed-bath">
-                                4 bed 4bath 150 sqm
+                                2bed  2bath 1190 sq ft
                             </div>
                             <div className="address">
-                                411 park Ave, SAN JOSE, CA 95110
+                                51 Innes Ct. #401, San Francisco, CA 94124
                             </div>
-                            <a className="btn btn-gradient" href={"/"}> Take a look
-                                <span className="dots"><i className="fas fa-ellipsis-h"></i></span>
-                            </a>
                     </div>
 
                     <div className="grid-item featured-house">
                         <img src={"../../../images/house2.jpg"} alt={""} className="house-image"/>
-                            <h3 className="sale-price">From $730k</h3>
+                            <h3 className="sale-price">$ 950,000</h3>
                             <div className="bed-bath">
-                                6 bed 4bath 210 sqm
+                                5 bed 2.5 bath 2250 sq ft
                             </div>
                             <div className="address">
-                                411 park Ave, SAN JOSE, CA 95110
+                                343 - 345 Russia Ave, San Francisco, CA 94112
                             </div>
-                            <a href="#" className="btn btn-gradient">Take a look
-                                <span className="dots"><i className="fas fa-ellipsis-h"></i></span>
-                            </a>
-                    </div>
+                     </div>
 
                     <div className="grid-item featured-house">
                         <img src = {"../../../images/house3.jpg"} alt={""} className="house-image"/>
-                            <h3 className="sale-price">From $350k</h3>
+                            <h3 className="sale-price">$ 7,995,000</h3>
                             <div className="bed-bath">
-                                4 bed 2bath 110 sqm
+                                4 bed 3.5 bath 4822 sq ft
                             </div>
                             <div className="address">
-                                411 park Ave, SAN JOSE, CA 95110
+                                1230 Sacramento St, San Francisco, CA 94108
                             </div>
-                            <a href={""} className="btn btn-gradient">Take a look
-                                <span className="dots"><i className="fas fa-ellipsis-h"></i></span>
-                            </a>
+
                     </div>
                 </div>
 
