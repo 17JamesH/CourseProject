38bd6a81f9d4dbcc3f7b44c47d5d17aae2117ba3
17JamesH <jameshe424@gmail.com>
2021-12-09 20:43:17 -0600

    Add updated web scraper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import csv
 import re
 import urllib
 import time
@@ -12,6 +13,33 @@ def get_js_soup(url, driver):
     res_html = driver.execute_script('return document.body.innerHTML')
     soup = BeautifulSoup(res_html,'html.parser') #beautiful soup object to be used for parsing html content
     return soup
+
+"""
+Gets a list of urls of houses in a certain area on RedFin. 
+
+Accepts url of a search of the area and the crawler driver as inputs
+Returns a pair with the url of the house as ID, and a document containining the
+house's features as text data.
+"""
+def getHouseUrls(filename):
+    urls = []
+    rows = []
+    with open(filename) as sanFranciscoData:
+        reader = csv.reader(sanFranciscoData)
+        for row in reader:
+            if row[0] == 'SALE TYPE':
+                continue
+            urls.append(row[20])
+            rows.append([len(rows) + 1, row[7], row[2], row[8], row[9], row[4], row[6], row[3]])
+    
+        with open('sqldatabase.csv', 'w', newline='') as sqldatabase:
+            writer = csv.writer(sqldatabase)
+            for row in rows:
+                writer.writerow(row)
+            
+    return urls
+
+
 
 """
 Gets text data from a specific house on RedFin. 
@@ -41,7 +69,16 @@ options = Options()
 options.headless = True
 driver = webdriver.Chrome('./chromedriver', options=options)
 
-# TODO: crawl redfin site for all properties in an area, and call getHouseData on each one
-testurl = 'https://www.redfin.com/CA/Los-Angeles/150-W-118th-St-90061/home/7322700'
+houseUrls = getHouseUrls('redfin_2021-12-05-16-23-32.csv')
+print(houseUrls)
 
-print(getHouseData(testurl, driver))
+i = 0
+with open('textData.txt', 'w') as dataFile:
+    for url in houseUrls:
+        i += 1
+        url, textdata = getHouseData(url, driver)
+        dataFile.write(str((i, url, textdata)))
+        dataFile.write('\n')
+        
+    dataFile.close()
+    
